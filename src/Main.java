70e856c1c3c9bd82cd5e7101a2744be6ee6d9a0d
Alexandr Yakovlev <engelsovich75@yandex.ru>
2022-12-04 22:43:30 +0500

    changing the comparator method

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,19 @@ public class Main {
     public static void main(String[] args) {
 
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Евгений", "Колотыгин", 46));
-        people.add(new Person("Сергей", "Заболотский", 45));
-        people.add(new Person("Константин", "Гасилов", 48));
-        people.add(new Person("Александр", "Яковлев", 47));
-        people.add(new Person("Николай", "Репин", 47));
-        people.add(new Person("Александр", "Аристов", 46));
-        people.add(new Person("Владимир", "Канашевский", 44));
+        people.add(new Person("Хосе Рауль", "Капабланка-и-Граупера", 22));
+        people.add(new Person("Евгений", "Зноско-Боровский", 26));
+        people.add(new Person("Александр", "Алехин", 18));
+        people.add(new Person("Федор", "Дуз-Хотимирский", 31));
+        people.add(new Person("Александр", "Ильин-Женевский", 16));
 
         System.out.println(people);
 
-        Collections.sort(people, new PersonsComparator(9));
+        Collections.sort(people, new PersonsComparator(3));
+
+        System.out.println(people);
+
+        Collections.sort(people, new PersonsComparator(2));
 
         System.out.println(people);
     }
